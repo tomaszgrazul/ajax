56a6d18afd3468aa2426ff7776bec7f773593450
tomaszgrazul <tgra@wp.pl>
2023-02-14 19:41:19 +0100

    zrobione

--- zad1_pobierz_dane/script.js
+++ zad1_pobierz_dane/script.js
@@ -1,17 +1,3 @@
-// $('.accordion-content').slideUp();
-// $(document).ready(function() {
-//     $('.accordion-item').click(function() {
-//         if($(this).hasClass('open')) {
-//             $(this).removeClass('open');
-//             $(this).find('.accordion-content').slideUp();
-//         } else {
-//             $(this).addClass('open');
-//             $(this).find('.accordion-content').slideDown();
-//         }
-//     });
-// });
-
-
 $(document).ready(function() {
     $('button').click(function() {
         $.getJSON('https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php', function(data) {
